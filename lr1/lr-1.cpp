f1a224bf87a32d97b26793437083bb729b96002b
Junaid Asif <115881388+junaidasif-dev@users.noreply.github.com>
2022-12-05 17:27:55 +0500

    Add files via upload

---
+++ lr1/lr-1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int apple=10,cost=5,total;
+	total=apple*cost;
+	cout<<"Total price of apples are "<<total;
+	getch();
+}
---
+++ lr1/lr-3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter an enteger: ";
+	cin>>a;
+	b=a+10;
+	cout<<a<<b;
+	getch();
+}
