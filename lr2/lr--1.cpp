93ca6ba03df8b6e611ada6eef7ae6a393b70d82c
Junaid Asif <115881388+junaidasif-dev@users.noreply.github.com>
2022-12-06 18:37:51 +0500

    Add files via upload

---
+++ lr2/lr--1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a,b,c,sum;
+	cout<<"Enter three integers:";
+	cin>>a>>b>>c;
+	sum=a+b+c;
+	cout<<"Sum = "<<sum;
+	getch();
+}
---
+++ lr2/lr--3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	char a;
+	int n;
+	cout<<"Enter any character:";
+	cin>>a;
+	n=a;
+	cout<<"\nThe ascii value of "<<a<<" is "<<n;
+	getch();
+}
